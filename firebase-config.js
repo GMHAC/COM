4368df1e36d68b18b8d01e2d0f687d4439212ab4
Sheraji <ssheraji@gmail.com>
2026-08-29 22:20:04 +0600

    Fix RFIMS Firebase Authentication login

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Firebase Web SDK configuration for rfims-s.web.app
-// Web config is not a service-account secret. Protect data with Auth + Firestore/Storage Rules.
+// Firebase Web SDK configuration for RFIMS
 window.RIZVI_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC3PFLDiu1aSZlneyeCYKfEPBrrmYMeQY4",
-  authDomain: "gen-lang-client-0506048076.firebaseapp.com",
-  projectId: "gen-lang-client-0506048076",
-  storageBucket: "gen-lang-client-0506048076.firebasestorage.app",
-  messagingSenderId: "351769372298",
-  appId: "1:351769372298:web:5a6a872b066ee58042ef73"
+  apiKey: "AIzaSyC8bzwNMgE8xRNC0WeBVNQtWFL1W3ylft0",
+  authDomain: "rfims-s.firebaseapp.com",
+  projectId: "rfims-s",
+  storageBucket: "rfims-s.firebasestorage.app",
+  messagingSenderId: "25755511409",
+  appId: "1:25755511409:web:864e272a8f57b22e2ec9d0"
 };
